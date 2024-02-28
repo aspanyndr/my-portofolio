7c76b7c4b63fb1406d254cd002655eaef573fdc0
aspanyndr <aspanyundar20@gmail.com>
2024-02-28 19:43:17 +0700

    adding component

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Education from './components/education/Education'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Foooter'
+import Nav from './components/nav/Nav'
+import Services from './components/services/Services'
+
+
+
+const App = () => {
+  return (
+    <>
+        <About />
+        <Contact />
+        <Education />
+        <Experience />
+        <Footer />
+        <Nav />
+        <Services />
+    </>
+    
+  )
+}
+
+export default App
